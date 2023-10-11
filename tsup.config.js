6b746bf38f55cff46ef2fe7aacc65e9406faae00
Nicole White <nicole@autoblocks.ai>
2023-10-11 17:05:16 -0400

    Get rid of tsup

--- tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts', 'src/langchain/index.ts'],
-  format: ['cjs', 'esm'],
-  sourcemap: true,
-  clean: true,
-  dts: true,
-});
