6b0719c2b2e093eae036ef245c89b75bbdfce160
Nicole White <nicole@autoblocks.ai>
2023-10-17 12:12:46 -0400

    Add openai and langchain as external

--- tsup.config.js
+++ tsup.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts', 'src/langchain/index.ts'],
   format: ['cjs', 'esm'],
+  external: ['openai', 'langchain'],
   sourcemap: true,
   clean: true,
   dts: true,
