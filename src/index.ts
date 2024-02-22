cc1f0d79d98668a86c9c9e306c1c87492aa1747d
Nicole White <nicole@autoblocks.ai>
2024-02-22 10:37:52 -0500

    Drop exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { AutoblocksTracer } from './tracer';
 export { AutoblocksAPIClient, SystemEventFilterKey } from './client';
 export type { View, Event, Trace } from './client';
-export { makeReplayHeaders, makeReplayHtmlUrl } from './util';
