db5d18f2e069e3a96060c1cabd51f4735909cae9
Nicole White <nicole@autoblocks.ai>
2024-02-28 11:10:23 -0500

    Rename CLI (#116)
    See https://github.com/autoblocksai/python-sdk/pull/126

--- bin/prompts-cli.js
+++ bin/prompts-cli.js
@@ -3,9 +3,8 @@
 const { run } = require('../dist/prompts-cli');
 
 async function main() {
-  // TODO: use something like yargs to parse args
-  // This is called via `autoblocks prompts generate`
-  if (process.argv[2] === 'prompts' && process.argv[3] === 'generate') {
+  // TODO: move this into the new CLI at autoblocksai/cli
+  if (process.argv[2] === 'generate') {
     await run();
   } else {
     console.error(`Unknown command: ${process.argv.slice(2).join(' ')}`);
