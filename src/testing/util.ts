73c45f4b6270ae029f46c40c465c8ab697b35257
Nicole White <nicole@autoblocks.ai>
2024-03-12 12:45:44 -0400

    Move /results & /evals reqs into unsafe methods (#119)
    Same as https://github.com/autoblocksai/python-sdk/pull/135

--- src/testing/util.ts
+++ src/testing/util.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 interface Waiter {
   done: boolean;
   wakeUp: () => void;
@@ -78,4 +80,28 @@ export class Semaphore {
       this.release();
     }
   }
+}
+
+export function isPrimitive(value: unknown): boolean {
+  return (
+    typeof value === 'string' ||
+    typeof value === 'number' ||
+    typeof value === 'boolean'
+  );
+}
+
+export function makeTestCaseHash<TestCaseType>(
+  testCase: TestCaseType,
+  testCaseHash:
+    | (keyof TestCaseType & string)[]
+    | ((testCase: TestCaseType) => string),
+): string {
+  if (Array.isArray(testCaseHash)) {
+    const concatenated = testCaseHash
+      .map((key) => JSON.stringify(testCase[key]))
+      .join('');
+    return crypto.createHash('md5').update(concatenated).digest('hex');
+  }
+
+  return testCaseHash(testCase);
 }
