df936c989819b0209730258a97ead5e8b1e53c4e
Nicole White <nicole@autoblocks.ai>
2024-04-16 18:00:26 -0400

    node:async_hooks (#137)

--- src/asyncLocalStorage.ts
+++ src/asyncLocalStorage.ts
@@ -1,4 +1,4 @@
-import { AsyncLocalStorage } from 'async_hooks';
+import { AsyncLocalStorage } from 'node:async_hooks';
 
 export const testCaseRunAsyncLocalStorage = new AsyncLocalStorage<{
   testCaseHash: string;
