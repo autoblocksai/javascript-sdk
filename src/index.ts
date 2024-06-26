2e95794031627034225a34bf61b07250e9f39eec
Nicole White <nicole@autoblocks.ai>
2024-06-26 09:35:34 -0400

    Use /prompts/types endpoint (#168)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { AutoblocksTracer, flush } from './tracer';
 export { AutoblocksAPIClient, SystemEventFilterKey } from './client';
 export type { View, Event, Trace } from './client';
+export type { TimeDelta } from './types';
