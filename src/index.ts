83150382bc476842cbd8c0e36c55d1644d611787
Adam Nolte <adam@autoblocks.ai>
2024-07-09 12:01:20 -0500

    Export dedent (#176)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { AutoblocksTracer, flush } from './tracer';
 export { AutoblocksAPIClient, SystemEventFilterKey } from './client';
+export { dedent } from './util';
 export type { View, Event, Trace } from './client';
 export type { TimeDelta } from './types';
