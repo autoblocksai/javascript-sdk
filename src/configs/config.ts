07deee17c816563ea64c970ba2dcfc7f4b9c490c
Luka Bura <luka.bura@autoblocks.ai>
2025-07-22 12:48:55 +0200

    fix: pipeline errors

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -47,7 +47,7 @@ const configRevisionsMap = (): Record<string, string> => {
 
 export class AutoblocksConfig<T> {
   private _value: T;
-  private refreshIntervalTimer: NodeJS.Timer | undefined;
+  private refreshIntervalTimer: ReturnType<typeof setInterval> | undefined;
 
   constructor(value: T) {
     this._value = value;
